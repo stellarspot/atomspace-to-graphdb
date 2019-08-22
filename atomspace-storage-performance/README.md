bd2926b2668da09cbd57c614227d95db3b38a843
Alexander Scherbatiy <stellarspot@gmail.com>
2019-08-22 18:36:53 +0300

    Create class and subclass in OWL

--- atomspace-storage-performance/README.md
+++ atomspace-storage-performance/README.md
@@ -2,3 +2,4 @@
 
 * [Animal Natural History and Life History Ontology](http://aber-owl.net/ontology/ADW/)
 * [Ontology Java parser OWLAPI](https://github.com/owlcs/owlapi/)
+* [Introduction OWLAPI](http://syllabus.cs.manchester.ac.uk/pgt/2017/COMP62342/introduction-owl-api-msc.pdf)
--- atomspace-storage-performance/src/main/java/sample/OntologyParserSample.java
+++ atomspace-storage-performance/src/main/java/sample/OntologyParserSample.java
@@ -2,8 +2,9 @@ package sample;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.formats.LabelFunctionalDocumentFormat;
+import org.semanticweb.owlapi.formats.RDFJsonDocumentFormat;
+import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
 
@@ -29,14 +30,30 @@ public class OntologyParserSample {
 
     private static void createOntology(OWLOntologyManager manager) throws Exception {
 
-        OWLOntology ontology = manager.createOntology();
-        ontology.saveOntology(new FunctionalSyntaxDocumentFormat(), System.out);
+        IRI IOR = IRI.create("http://owl.api.tutorial");
 
+        OWLOntology ontology = manager.createOntology();
+
+        System.out.printf("axioms: %d, format: %s%n", ontology.getAxiomCount(), manager.getOntologyFormat(ontology));
+
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        OWLClass person = factory.getOWLClass(IOR + "#Person");
+        OWLDeclarationAxiom personAxiom = factory.getOWLDeclarationAxiom(person);
+
+        ontology.add(personAxiom);
+
+        OWLClass woman = factory.getOWLClass(IOR + "#Woman");
+        OWLSubClassOfAxiom womanAxiom = factory.getOWLSubClassOfAxiom(woman, person);
+        ontology.add(womanAxiom);
+
+        ontology.saveOntology(new RDFJsonDocumentFormat(), System.out);
     }
 
     private static void loadOntology(OWLOntologyManager manager, String ontologyFile) throws Exception {
         System.out.printf("Ontology file: %s%n", ontologyFile);
         OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(ontologyFile));
         System.out.printf("ontology size: %d%n", manager.ontologies().count());
+        System.out.printf("axioms: %d, format: %s%n", ontology.getAxiomCount(), manager.getOntologyFormat(ontology));
+        ontology.saveOntology(new RDFJsonDocumentFormat(), System.out);
     }
 }
