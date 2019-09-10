61e4a864f46a8feb01f13dd05c4c3b62e6c65675
Alexander Scherbatiy <stellarspot@gmail.com>
2019-09-10 15:08:05 +0300

    Update JanusGraph sample

--- atomspace-storage-performance/src/main/java/sample/janusgraph/JanusGraphExample.java
+++ atomspace-storage-performance/src/main/java/sample/janusgraph/JanusGraphExample.java
@@ -2,11 +2,9 @@ package sample.janusgraph;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.janusgraph.core.*;
 import org.janusgraph.core.schema.JanusGraphManagement;
-
-import java.util.Map;
 
 public class JanusGraphExample {
 
@@ -30,19 +28,40 @@ public class JanusGraphExample {
         System.exit(0);
     }
 
-    private static void sample(JanusGraph graph) {
-        JanusGraphTransaction tx = graph.newTransaction();
-        // vertices
-
-        Vertex saturn = tx.addVertex("type", "titan", "name", "saturn", "age", 10000);
-        tx.commit();
-    }
+    private static final String SUBJECT_LABEL = "SubjectNode";
+    private static final String OBJECT_LABEL = "ObjectNode";
+    private static final String PREDICATE_LABEL = "PREDICATE";
 
     private static void addPredicate(JanusGraph graph, String subject, String predicate, String object) {
 
+        JanusGraphManagement management = graph.openManagement();
+
+        if (!management.containsVertexLabel(SUBJECT_LABEL)) {
+            management.makeVertexLabel(SUBJECT_LABEL).make();
+        }
+
+        if (!management.containsVertexLabel(OBJECT_LABEL)) {
+            management.makeVertexLabel(OBJECT_LABEL).make();
+        }
+
+        if (!management.containsEdgeLabel(PREDICATE_LABEL)) {
+            management.makeEdgeLabel(PREDICATE_LABEL).make();
+        }
+
+        management.commit();
+
         JanusGraphTransaction tx = graph.newTransaction();
-        tx.addVertex("Subject").property("value", subject);
-        tx.addVertex("Object").property("value", object);
+
+
+        JanusGraphVertex sbj = tx.addVertex(SUBJECT_LABEL);
+        sbj.property("value", subject);
+
+        JanusGraphVertex obj = tx.addVertex(OBJECT_LABEL);
+        obj.property("value", object);
+
+
+        JanusGraphEdge edge = sbj.addEdge(predicate.toUpperCase(), obj);
+
         tx.commit();
     }
 
@@ -55,10 +74,18 @@ public class JanusGraphExample {
     private static void print(JanusGraph graph) {
         System.out.printf("--- dump graph ---%n");
         GraphTraversalSource g = graph.traversal();
-        GraphTraversal<Vertex, Map<Object, Object>> properties = g.V().valueMap();
 
-        while (properties.hasNext()) {
-            System.out.printf("values: %s%n", properties.next());
+        GraphTraversal<Edge, Edge> edges = g.E();
+        while (edges.hasNext()) {
+            Edge edge = edges.next();
+            System.out.printf("edge: %s%n", edge);
+            String startLabel = edge.inVertex().label();
+            String startValue = edge.inVertex().value("value");
+            String endLabel = edge.outVertex().label();
+            String endValue = edge.outVertex().value("value");
+            String edgeLabel = edge.label();
+            System.out.printf("(:%s {value: '%s'}) - [%s] -> (:%s {value: '%s'})%n",
+                    startLabel, startValue, edgeLabel, endLabel, endValue);
         }
         System.out.printf("--- ---------- ---%n");
     }
